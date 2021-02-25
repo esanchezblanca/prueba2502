44cb7d9d84481bddeaedfd0aa47e28a962f8a537
esanchezblanca <ele.sp@hotmail.com>
2021-02-25 20:02:29 +0100

    modificado puerto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require ('cors');
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8080
 
 const apiRouter = require('./routes/api');
 
